30fc3fa465249d260f0699b1839d48641fcaba1a
unknown <jonahmungainyokabi@gmail.com>
2024-07-25 13:53:19 +0300

    added whitenoise package to handle static files in prod mode

--- Bookstore/Bookstore/settings.py
+++ Bookstore/Bookstore/settings.py
@@ -58,6 +58,11 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',#serving startic files in prod # deployed on koyeb
+
+
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -188,7 +193,7 @@ STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),
 STATIC_ROOT = os.path.join(BASE_DIR,  'static_media/') #prev deploy b4
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"# for production mode
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
